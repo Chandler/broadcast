59e16dbb9f95e6d87504c7b01ec8c0b2606dd908
Chandler Abraham <cabraham@gmail.com>
2013-06-19 21:38:51 -0700

    removed configurable errors

--- broadcast.rb
+++ broadcast.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
-require 'time_difference'
+require 'psych' #yaml
+SALUTATION               = "#RWEN"
+PERMISSION_ERROR         = "Ah ah ah, you didn't say the magic word"
+RATE_LIMIT_ERROR         = "oops, you already used a broadcast this week. Don't be selfish"
+DELIVERY_FAIL_ERROR      = "fail_whale.jpg : /"
 
-config = YAML.load_file('config.yml').symbolize_keys
-twilio_config = config[:twilio]
-members       = config[:members]
-constants     = config[:constants]
+config = Psych.load_file('config.yml')
+puts config.inspect
+twilio_config = config['twilio']
+members       = config['members']
 
-@client = Twilio::REST::Client.new twilio_config[:account_sid], twilio_config[:auth_token]
+@client = Twilio::REST::Client.new twilio_config['account_sid'], twilio_config['auth_token']
 
 get '/incoming' do
   message       = params[:Body]
@@ -16,9 +20,9 @@ get '/incoming' do
   sender_name   = members[sender_number]
 
   if !!sender
-    response = constants[:PERMISSION_ERROR]
+    response = PERMISSION_ERROR
   elsif is_over_message_limit(sender)
-    response = constants[:RATE_LIMIT_ERROR]
+    response = RATE_LIMIT_ERROR
   else #lgtm let's do this
     response = message_everyone(phone_number, message)
   end
@@ -28,7 +32,7 @@ end
 
 def message_everyone sender
   message = message[0..320] #max length two text messages.
-  message = "@#{sender}: " + message + "- #{constants[:SALUTATION]}"
+  message = "@#{sender}: " + message + "- #{SALUTATION}"
   successful_deliveries = 0
   
   begin
@@ -37,9 +41,9 @@ def message_everyone sender
       successful_deliveries = successful_deliveries = + 1
     end
   rescue Twilio::Rest::RequestError => e
-      return successful_deliveries > 0 ? constants[:DELIVERY_PARTIAL_SUCCESS] : constants[:DELIVERY_FAIL_ERROR]
+      return successful_deliveries > 0 ? "Something blew up, but the message was still delivered to #{successful_deliveries} friends" : DELIVERY_FAIL_ERROR
   end
-  constants[:DELIVERY_SUCCESS]
+  return "Great success, your message was delivered to #{successful_deliveries} friends"
 end
 
 def send_message(message, recipient_number)
@@ -47,14 +51,14 @@ def send_message(message, recipient_number)
   @client.account.sms.messages.create(
     :body => message,
     :to =>   recipient_number,
-    :from => twilio_config[:from_number])
+    :from => twilio_config['from_number']
   )
 end
 
 def is_over_message_limit sender
   message_record = load_message_record || {}
   last_message_time = message_record[sender]
-  if !!last_message_time return false
+  return false if !!last_message_time 
 
   delta = Time.now.to_i - last_message_time.to_i
   delta < config[:rate_limit].to_i
@@ -67,5 +71,5 @@ def update_record sender, timestamp
 end
 
 def load_message_record
- YAML.load_file('message_record.yml').symbolize_keys
+ Psych.load_file('message_record.yml').symbolize_keys
 end
